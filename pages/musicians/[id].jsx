0764281ac4a7a524fdbc1a67fc8921a2c59d7102
SAID BIO WEDE <saidbiowede@gmail.com>
2022-03-25 12:24:41 +0100

    seconde

--- pages/musicians/[id].jsx
+++ pages/musicians/[id].jsx
@@ -19,6 +19,7 @@ export default function Musician() {
                     <div className={styles.socials}>
                         <div className={styles.social}>
                             <AiFillInstagram />
+                            
                         </div>
                         <div className={styles.social}>
                             <AiOutlineTwitter />
